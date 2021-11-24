e97d6205ab2e2bb7686606825c66e731f16bad0d
Julie Judachina <iuliia.saunova@email.cz>
2021-11-24 08:48:04 +0100

    add embla slider

--- src/slide.js
+++ src/slide.js
@@ -1,4 +1,13 @@
-var emblaNode = document.querySelector('.embla');
-var options = { loop: false };
+// Grab wrapper nodes
+const rootNode = document.querySelector('.embla');
+const viewportNode = rootNode.querySelector('.embla__viewport');
 
-var embla = EmblaCarousel(emblaNode, options);
+// Grab button nodes
+const prevButtonNode = rootNode.querySelector('.embla__prev');
+const nextButtonNode = rootNode.querySelector('.embla__next');
+
+const embla = EmblaCarousel(viewportNode);
+
+// Add click listeners
+prevButtonNode.addEventListener('click', embla.scrollPrev, false);
+nextButtonNode.addEventListener('click', embla.scrollNext, false);
